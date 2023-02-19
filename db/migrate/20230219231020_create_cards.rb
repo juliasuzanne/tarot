090608a03a72a2633ba09459c49f4d8d4f5d25cf
Julia Grimes <julia.s.grimes@gmail.com>
2023-02-19 18:11:16 -0500

    initial commit with card model

---
+++ db/migrate/20230219231020_create_cards.rb
@@ -0,0 +1,12 @@
+class CreateCards < ActiveRecord::Migration[7.0]
+  def change
+    create_table :cards do |t|
+      t.string :name
+      t.string :description
+      t.string :image
+      t.string :category
+
+      t.timestamps
+    end
+  end
+end
