7511f1e5fd6245c0ab0fecaf7e03fc6cf468f3d5
Julia Grimes <julia.s.grimes@gmail.com>
2023-02-19 18:44:23 -0500

    Added and migrated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,113 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Card.create(name: "The High Priestess", description: "Ruled by the Moon (and Cancer by default), she represents the Water Element. She embodies our innate knowing and wisdom, our intuitive and higher selves. She practically transforms into water at the end of her gown, reinforcing her one with its transmutability and power. Her crown shows three phases of the Moon, waxing, full, and waning, reflecting the so-called three stages of womanhood- maiden, mother, crone, and that she is equally potent in each. With this card, your internal wellspring of knowledge is sacred, listen to it.", category: "Major Arcana")
+
+# Card.create(name: "The Empress", description: "Ruled by Venus (and Taurus and Libra by default), The Empress is an embodiment of the Earth element. She is our mother-like presence, whether internal or external, our ability to nourish ourselves truly and deeply. She is everything fertile, whether that be a literal pregnancy or the conception of a project, her creative power is strong and the capacity to nurture it successfully to completion even stronger. She knows that self-care and indulging in the finer things in life, whatever makes your soul sing, is sacred and worth protecting. ", category: "Major Arcana")
+
+# Card.create(name: "The Emperor", description: "This is the first Tarot card to be directly ruled by a Zodiac Sign, Aries (which is also the first Sign in the Astrological cycle). This makes him a Fire Element. The Emperor, counterpart to The Empress after we have learned to properly take care of ourselves, asks us to boldly take up space in the world and shine our light, speak our truth, and lead the way on our path fiercely.", category: "Major Arcana")
+
+# Card.create(name: "The Hierophant", description: "This is the card for the year 2021 (read more about it in our post here). It is Taurus’ card and so is symbolic of the Earth Element. The Hierophant teaches us to be our own spiritual wisdom keepers and share our knowledge with others. It asks to make sure where you are getting your beliefs, thoughts, opinions, ideas, feelings, and any other teachings from is in alignment with your highest self, while not giving away your personal power. Only you know what is best for you and what your truth is. Perhaps you are being called to explore your spirituality more or teach others about your ways.", category: "Major Arcana")
+
+# Card.create(name: "The Lovers", description: "This is Gemini’s card and representative of the Air Element. The Lovers can be indicative of a new relationship or deepening existing one, but always with the intention of embracing and integrating the inherent duality present in everyone, you are yours before you are anyone else’s and you are always whole. No one can “complete” you, the person you are looking for is you- as they say, you’ll find the one when you realise that you are the one. The most important relationship you will ever have in life is first and foremost the one you have with yourself, and from this will stem everything else. How can you accept all sides of yourself, including your “shadow side” and parts that want healing? We are in a constant state of flux, balancing Yin and Yang, The Lovers card reminds us of this. What you love in someone else is what you love about yourself, and likewise what you judge others for is what you judge yourself for. What do your triggers have to teach you?", category: "Major Arcana")
+
+# Card.create(name: "The Chariot", description: "This is Cancer’s card (check out our post on Zodiac Signs for more information) and thus a Water Element card. The Chariot asks us to be emotionally brave and stable, and persevere with strength through what we are trying to endure, the end is almost in sight!", category: "Major Arcana")
+
+# Card.create(name: "Strength", description: "This is Leo’s card and so representative of the Fire Element. As we can see in the picture of Strength, this is not brute force, but rather the deep, enduring inner strength we each possess, the one that is not loud and shouting, but soft and confident. She, too, has the infinity symbol above her head (like The Magician), symbolic of her eternal courage, our internal fountain of bravery knows no limits. As she is gently opening the lion’s mouth, we can see that it is in moments of tender vulnerability when we are in fact strongest. She is not dressed in fighting amour, nor charging ahead, but is quietly composed, steadfast in the knowledge of her capabilities. Where is your inner voice roaring when it should perhaps be more confident as a whisper?", category: "Major Arcana")
+
+# Card.create(name: "The Hermit", description: "This is Virgo’s card and representative of the Earth Element. The Hermit is a card of deep and reverent introspection and self-reflection. It asks to step away from the noise of the world and retreat to our inner world to hear what our soul is saying. It is only through moments of stillness that can gain enough time for our internal wisdom to speak. Where will your shining star guide you, what is it illuminating? If you pull this card it is an invitation for some sacred time alone to explore yourself.", category: "Major Arcana")
+
+# Card.create(name: "Wheel of Fortune", description: "This is ruled by Jupiter (and Sagittarius by default), representing the Fire Element. The Wheel of Fortune depicts the ever-turning cycles of life, one moment we are on top, the next we are at the bottom, nothing is forever and everything holds its place, none are better or worse than the other- they each hold their own lessons and medicine. Generally, as Jupiter is a positive planet, it signals good luck, but is still a reminder to enjoy the present moment as best you can, remaining aware of the impermanent nature of things.", category: "Major Arcana")
+
+# Card.create(name: "Justice", description: "This is Libra’s card and reflective of the Air Element (it is also a “master number”, numerology is extremely important in the Tarot, click through to read more about The Link Between Numerology and Tarot). The Justice card, as the name suggests, relates to all things judicial, moral, and ethical, dealing with things of fairness, rightness, and equality. It can be an indicator to check in with your own prejudices or a symbol of your righteous integrity.", category: "Major Arcana")
+
+# Card.create(name: "The Hanged Man", description: "This is ruled by Neptune (and Pisces by default) representing its liminal quality, and is by consequence a Water Element card. The Hanged Man is a call to wait, pause, let the unfoldings of life to naturally unfurl themselves without interference. Don’t force something to happen that doesn’t want to be, allow things to occur on their own in their own time. As the man is hanging (placidly) upside down, it may also indicate a change in perspective or point of view. All will be revealed in due time, but for now, float easily in the waters of life’s tides.
+
+# ", category: "Major Arcana")
+
+# Card.create(name: "Death", description: "This is Scorpio’s card and so also representative of the Water Element. Death is a natural part of life, a necessary part of life. Without it, nothing fresh would be able to come through, in order for the new to be born, the old must first die. If you pull the Death card it can indicate that you are in a powerful transition of metamorphosis and change, slowly shedding your skin to emerge refreshed and renewed, born again from the chrysalis a transformed being.", category: "Major Arcana")
+
+# Card.create(name: "Temperance", description: "This is Sagittarius’ card and so ruled by the Fire Element. Temperance is the card of Divine timing and patience, a lesson to consume everything in moderation, practice grace with yourself, others, life. Where can you soften your approach and ease into the effortless, mild capabilities of the angel on the card to seamlessly water-bend. How can you embrace your inner sanctuary of peace and trust in the natural process of things while holding onto a sense of serenity and tranquility, comfortable in not having it all at once? Is there somewhere your internal fire needs to be tamed or restrained in some way?", category: "Major Arcana")
+
+# Card.create(name: "The Devil", description: "This is Capricorn’s card and so an Earth Element. The Devil is representative of our Earthly vices, those things, habits or thought patterns we become obsessed with that don’t serve our best and highest good. It can also point to too much importance placed on material objects or things outside of ourselves, as well as downright addiction.
+
+#   ", category: "Major Arcana")
+
+# Card.create(name: "The Tower", description: "This is ruled by Mars (and Aries by default) with a Fire Element disposition. It’s safe to say that this card is fiery. The Tower represents the crumbling of false foundations, collapse of unstable beliefs, and the cataclysmic destruction of anything that was created on shaky ground. Sometimes this can relate to things that previously served us that have run their course, other times it is the seismic shifts of something that should have never been erected in the first place. Whatever it is, The Tower brings change. This may also be a lesson we didn’t take heed to in the past, which could have more naturally, but not necessarily less painfully (such as with Death), shifted in its own due time that we chose to ignore. What may have started as a whisper is now a rumbling thunder and clap of lightening that will force change upon us whether we want it or not. However, that’s not to say that The Tower has to be difficult, it may be something you were aware needed to evolve, or it could be a small thing that has run its course.", category: "Major Arcana")
+
+# Card.create(name: "The Star", description: "Ahhh sweet release, after all those growing pains and the potential chaos of the previous cards, much like the quiet after a storm, we have the beautiful blessing of the Star of hope. This is Aquarius’ card, who is always looking to the future with faith and regeneration, and so also an Air Element card. Like a breath of fresh air, we can breath easy in the breeze of The Star, safe in the knowledge that everything is as it should be and that we are on the right path and Divinely protected. Take this invitation to rest and reset, enjoying the nectar of the present moment.", category: "Major Arcana")
+
+# Card.create(name: "The Moon", description: "This is Pisces’ card, a Water Element. It deals with illusions and false ideas, as well as the hidden and unknown. Use your intuition to guide you through the night and illuminate any lingering things that are holding you back. Perhaps now, refreshed and regenerated, we are able to see some things more clearly in hindsight. It can also point to a clouded judgement, foggy mind, or secrets that are yet to pass.", category: "Major Arcana")
+
+# Card.create(name: "The Sun", description: "This is, of course, ruled by the Sun (and Leo by default), representing the Fire Element. Like all the other cards in the Tarot, when you pull The Sun, it does not necessarily mean that your life is, or will be, full of vitality, vibrancy, abundance, and joy (although it certainly can do!) but rather it asks where can you look to find these things and appreciate them, revelling in the gratitude. If you get The Sun card and your life is full of happiness and success then awesome! Hope for more of the same. However, if you pull it and you do not currently experience these things, it is a gentle reminder that brighter days are always coming and what can you see that you are surrounded with right now that makes you feel full? It can be as simple as a beautiful sunny day, a warm friend, a delicious cup of coffee that you can afford to treat yourself with. Abundance is all around us if we look.
+
+# ", category: "Major Arcana")
+
+# Card.create(name: "Judgement", description: "This is Pluto’s card, the generational planet (and Scorpio by default), representing the Water Element. As we approach the end of our major cycle in The Fool’s jouney, we are tasked with looking back on our progress and taking stock of our change, trials and tribulations, all that we have learnt and expressed in the world, the legacy that we leave behind in our wake. Take an honest look and congratulate yourself on how far you’ve come (which is often further than we give ourselves credit for). Look to where you were a month ago, a year ago- how have you grown? What do you leave behind you and how have you affected those around you? It also asks us to completely own all aspects of ourselves and the part we played in every situation we have been in, good and bad. How did we get there, what would we do differently now?", category: "Major Arcana")
+
+# Card.create(name: "The World", description: "This is Saturn’s card (and Capricorn’s by default), ruled by the Earth Element. We have finally come the end of the cycle. We can rejoice and appreciate, soaking up all the lessons we have learnt and gifts we have gained, ready to begin anew in the next cycle of life. Allow satisfaction to wash over you, thank your experiences for all they have taught you, and rest assured in this newfound growth.", category: "Major Arcana")
+
+# Card.create(name: "Ace of Wands", description: "New beginnings, start of projects, creative insight ", category: "Wands")
+# Card.create(name: "Two of Wands", description: "Decisions, travel, planning", category: "Wands")
+# Card.create(name: "Three of Wands", description: "Progress, expansion, looking to the future", category: "Wands")
+# Card.create(name: "Four of Wands", description: "Stability, harmony, happiness, family", category: "Wands")
+# Card.create(name: "Five of Wands", description: "Disagreements, bickering, opposing views", category: "Wands")
+# Card.create(name: "Six of Wands", description: "Victory, success, achievement", category: "Wands")
+# Card.create(name: "Seven of Wands", description: "Perseverance, holding your ground", category: "Wands")
+# Card.create(name: "Eight of Wands", description: "Action, movement, fast-pace ", category: "Wands")
+# Card.create(name: "Nine of Wands", description: "Recuperate, defence, resilience", category: "Wands")
+# Card.create(name: "Ten of Wands", description: " Burdens, over-pressured, hard work", category: "Wands")
+# Card.create(name: "Page of Wands", description: "News, exploration, excitement, freedom
+#   ", category: "Wands")
+# Card.create(name: "Knight of Wands", description: "Energy, passion, fearlessness
+#   ", category: "Wands")
+# Card.create(name: "Queen of Wands", description: "Exuberance, confidence, independence, joy
+#   ", category: "Wands")
+# Card.create(name: "King of Wands", description: "Leadership, vision, charisma
+#   ", category: "Wands")
+
+# Card.create(name: "Ace of Cups", description: "New relationships, compassion, creativity", category: "Cups")
+# Card.create(name: "Two of Cups", description: "Partnerships, unity, connection", category: "Cups")
+# Card.create(name: "Three of Cups", description: "Joy, celebration, community", category: "Cups")
+# Card.create(name: "Four of Cups", description: "Apathy, boredom, contemplation", category: "Cups")
+# Card.create(name: "Five of Cups", description: "Grief, loss, disappointment, pity, appreciation", category: "Cups")
+# Card.create(name: "Six of Cups", description: "Childhood, nostalgia, happiness, innocence", category: "Cups")
+# Card.create(name: "Seven of Cups", description: "Choice, confusion, decisions, illusions", category: "Cups")
+# Card.create(name: "Eight of Cups", description: "Moving on, growth, new pursuits", category: "Cups")
+# Card.create(name: "Nine of Cups", description: "Satisfaction, accomplishment, contentment", category: "Cups")
+# Card.create(name: "Ten of Cups", description: "Joy, security, contentment, belonging", category: "Cups")
+# Card.create(name: "Page of Cups", description: "New creative opportunities, growing emotions or intuition", category: "Cups")
+# Card.create(name: "Knight of Cups", description: "Emotionally perseverant, steadfast, romance, idealism", category: "Cups")
+# Card.create(name: "Queen of Cups", description: "Intuitive, emotionally stable and open, compassionate", category: "Cups")
+# Card.create(name: "King of Cups", description: "Steadfast, diplomatic, emotionally balanced", category: "Cups")
+
+# Card.create(name: "Ace of Swords", description: "New insight, start of projects", category: "Swords")
+# Card.create(name: "Two of Swords", description: "Decisions, intuition vs logic", category: "Swords")
+# Card.create(name: "Three of Swords", description: "Heartache, pain", category: "Swords")
+# Card.create(name: "Four of Swords", description: "Rest, regeneration, contemplation", category: "Swords")
+# Card.create(name: "Five of Swords", description: "Defeat, conflict, competition", category: "Swords")
+# Card.create(name: "Six of Swords", description: "Moving on, travel, change", category: "Swords")
+# Card.create(name: "Seven of Swords", description: "Deceit, lies, self-betrayal", category: "Swords")
+# Card.create(name: "Eight of Swords", description: "Mental prison, victim mentality ", category: "Swords")
+# Card.create(name: "Nine of Swords", description: "Anxiety, worry, mind traps", category: "Swords")
+# Card.create(name: "Ten of Swords", description: "Destruction, rock bottom, new beginnings", category: "Swords")
+# Card.create(name: "Page of Swords", description: "Quick insight, curiosity, restlessness", category: "Swords")
+# Card.create(name: "Knight of Swords", description: "Assertive, fast-paced, forceful", category: "Swords")
+# Card.create(name: "Queen of Swords", description: "Wit, composure, strategic, direct", category: "Swords")
+# Card.create(name: "King of Swords", description: "Detached, level-headed, disciplined ", category: "Swords")
+
+# Card.create(name: "Ace of Pentacles", description: "New monetary incomes, prosperity", category: "Pentacles")
+# Card.create(name: "Two of Pentacles", description: "Balance, juggling, priorities", category: "Pentacles")
+# Card.create(name: "Three of Pentacles", description: "Teamwork, craftsmanship, collaboration", category: "Pentacles")
+# Card.create(name: "Four of Pentacles", description: "Teamwork, craftsmanship, collaboration", category: "Pentacles")
+# Card.create(name: "Five of Pentacles", description: "Financial loss, spiritual poverty", category: "Pentacles")
+# Card.create(name: "Six of Pentacles", description: "Generosity, charity, exchange ", category: "Pentacles")
+# Card.create(name: "Seven of Pentacles", description: "Fruition, patience", category: "Pentacles")
+# Card.create(name: "Eight of Pentacles", description: "Craftsmanship, diligence, talent", category: "Pentacles")
+# Card.create(name: "Nine of Pentacles", description: "Luxury, indulgence, independence, success", category: "Pentacles")
+# Card.create(name: "Ten of Pentacles", description: "Legacy, family, contentment, fulfilment", category: "Pentacles")
+# Card.create(name: "Page of Pentacles", description: "New financial opportunities, skill development", category: "Pentacles")
+# Card.create(name: "Knight of Pentacles", description: "Hard work, responsibility, slow progress, efficiency", category: "Pentacles")
+# Card.create(name: "Queen of Pentacles", description: "Financial stability, nurturing, practical, grounded", category: "Pentacles")
+# Card.create(name: "King of Pentacles", description: "Abundant, wealthy, secure, prosperous ", category: "Pentacles")
